dbd350c97ebfd2b6bbc9464f1a67d98555a5624e
visua77 <nicznet@hotmail.com>
2020-03-09 21:47:04 +0100

    first commit of backend

--- server.js
+++ server.js
@@ -1,6 +1,54 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import { restart } from 'nodemon'
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/blogPosts1"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+// Setup model
+const Thought = mongoose.model('Thought', {
+
+    headline: {
+        type: String,
+        required: true,
+        minlenght: 5,
+        maxlenght: 140
+    },
+    text: {
+        type: String,
+        required: true,
+        minlenght: 5,
+        maxlenght: 340
+    },
+    imgName: {
+        type: String,
+        required: true,
+        minlenght: 5,
+        maxlenght: 140
+    },
+    message: {
+        type: String,
+        required: true,
+        minlenght: 5,
+        maxlenght: 140
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    tag: {
+        type: String
+    },
+    hearts: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+
+})
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -14,11 +62,35 @@ app.use(cors())
 app.use(bodyParser.json())
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+app.get('/', async(req, res) => {
+    const thoughts = await Thought.find().limit(20).sort({ createdAt: 'desc' })
+    res.json(thoughts)
 })
+
+app.post('/', async(req, res) => {
+    const thought = new Thought(req.body)
+    try {
+
+        const saved = await thought.save()
+        res.status(201).json(saved)
+    } catch (err) {
+        res.status(400).json({ message: 'could not save thought', errors: err.errors })
+    }
+})
+
+app.post('/:id/like', async(req, res) => {
+    try {
+        const thought = await Thought.findOneAndUpdate({ _id: req.params.id }, { $inc: { hearts: 1 } })
+        res.status(200).json(thought)
+    } catch (err) {
+        res.status(400).json({ message: 'could not update heart' })
+
+    }
+})
+
+
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
+    console.log(`Server running on http://localhost:${port}`)
 })
